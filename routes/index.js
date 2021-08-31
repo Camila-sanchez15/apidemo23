bc911bfdca82a53d2965c440366c3821df9fd90e
Camila-sanchez15 <84477245+Camila-sanchez15@users.noreply.github.com>
2021-08-31 19:58:50 -0300

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Delicias Lunaticas' });
+});
+
+module.exports = router;
